3074d99a9c5b65eaf965f88c0f5621d317a91bc9
akomborero <makomborerichidzviva@gmail.com>
2026-02-11 18:20:48 +0200

    changes

--- app/components/cardetails.tsx
+++ app/components/cardetails.tsx
@@ -113,7 +113,7 @@ export default function CarDetails({ carId }: CarDetailsProps) {
   const handleWhatsAppDirect = () => {
     if (!car) return;
     const myNumber = "27 78 759 3914"; 
-    const message = ` *New Inquiry: ${car.year} ${car.make} ${car.model}*%0A%0AHi Qaulity import cars.! I am interested in this vehicle...%0A%0A*Link:* ${window.location.href}`;
+    const message = ` *New Inquiry: ${car.year} ${car.make} ${car.model}Hi Qaulity import cars.! I am interested in this vehicle.. ${window.location.href}`;
     window.location.href = `whatsapp://send?phone=${myNumber}&text=${encodeURIComponent(message)}`;
   };
 
